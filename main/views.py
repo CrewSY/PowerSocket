1318e3f7d8a6a6eb32000db3c55f82fbd2477a97
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-02-20 17:21:49 +0200

    Create view for home page

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
 """Views for main app of PowerSocket project."""
 
-from django.shortcuts import render  # noqa: F401
+from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Smartphone
+
+
+def smartphones_list(request):
+    """Render page with goods list."""
+    smartphones = Smartphone.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
+    return render(request, 'main/smartphones_list.html', {'smartphones': smartphones})

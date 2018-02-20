f74577596630c92a60efc3fb6878c3fbbda0f5de
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-02-20 17:19:30 +0200

    Create Smartphone model
    -Also function str()

--- main/models.py
+++ main/models.py
@@ -1,3 +1,24 @@
 """Models for PowerSocket project."""
 
-from django.db import models  # noqa: F401
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import ugettext as _
+
+
+class Smartphone(models.Model):
+    """Model that represents goods."""
+
+    photo = models.ImageField(upload_to='static/img/',
+                              verbose_name=_(u'Photo'),
+                              blank=True,
+                              null=True)
+    title = models.CharField(verbose_name=_(u'Title'),
+                             max_length=200)
+    description = models.TextField(verbose_name=_(u'Text'))
+    price = models.FloatField(verbose_name=_(u'Price'))
+    publish_date = models.DateTimeField(verbose_name=_(u'Created date'),
+                                        default=timezone.now)
+
+    def __str__(self):
+        """Render the smartphone instance as a string."""
+        return self.title

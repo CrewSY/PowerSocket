d9a3f96ed3fb2c965ff3fe86aaf09bf2fe7638ae
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-02-22 15:05:28 +0200

    Improve Smartpone model
      - Add class SmartphonesBrands, thats represents existing brands
      - Add also field model
      - Delete field title. Instead title = brand+model
      - Refactor str function

--- main/models.py
+++ main/models.py
@@ -2,23 +2,38 @@
 
 from django.db import models
 from django.utils import timezone
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 
 class Smartphone(models.Model):
-    """Model that represents goods."""
+    """Model that represents smartphones."""
 
-    photo = models.ImageField(upload_to='static/img/',
-                              verbose_name=_(u'Photo'),
+    photo = models.ImageField(verbose_name=_(u'Photo'),
                               blank=True,
                               null=True)
-    title = models.CharField(verbose_name=_(u'Title'),
-                             max_length=200)
-    description = models.TextField(verbose_name=_(u'Text'))
+    brand = models.ForeignKey('SmartphonesBrands',
+                              verbose_name=_(u'Brand'),
+                              blank=False,
+                              null=True,
+                              on_delete=models.PROTECT)
+    model = models.CharField(verbose_name=_(u'Model'),
+                             max_length=255)
+    description = models.TextField(verbose_name=_(u'Description'))
     price = models.FloatField(verbose_name=_(u'Price'))
     publish_date = models.DateTimeField(verbose_name=_(u'Created date'),
                                         default=timezone.now)
 
     def __str__(self):
         """Render the smartphone instance as a string."""
-        return self.title
+        return u'%s %s' % (self.brand, self.model)
+
+
+class SmartphonesBrands(models.Model):
+    """Model that represents brands of smartphones."""
+
+    brand_name = models.CharField(verbose_name=_(u'Brand'),
+                                  max_length=64)
+
+    def __str__(self):
+        """Render the brand instance as a string."""
+        return self.brand

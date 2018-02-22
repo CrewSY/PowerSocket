e990e93919671ae36b4c5b308a3eeaf3968cc213
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-02-22 15:08:59 +0200

    Register SmartphonesBrands model on admin page

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 """Management of admin panel."""
 
 from django.contrib import admin
-from .models import Smartphone
+from .models import Smartphone, SmartphonesBrands
 
 admin.site.register(Smartphone)
+admin.site.register(SmartphonesBrands)

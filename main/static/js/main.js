d2bdd4cbbc27c2c48be8231eb9cd5b3ea55d2569
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-02-24 16:25:54 +0200

    Add check after reload for exist cookie
      - After reload page, check for exist cookie and add active class
        for choosed button

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -1,26 +1,23 @@
-
 function initBrandSelector() {
-  $('.tab button').click(function(e){
-    e.preventDefault();
-
+  $('.tab button').click(function(){
     var brand = $(this).val();
     if (brand) {
       $.cookie('current_brand', brand, {'path': '/', 'expires': 365});
-
     } else {
       $.removeCookie('current_brand', {'path': '/'});
     }
-
     location.reload(true);
-
-    $(this).addClass("active")
-
     return true;
   });
 }
 
 
 $(document).ready(function(){
+  var brand = $.cookie('current_brand');
+  if (brand) {
+    $('.tab button[value=' + brand + ']').addClass("active");
+  }
+
   initBrandSelector();
 
 });

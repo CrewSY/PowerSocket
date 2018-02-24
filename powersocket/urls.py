a31480c3816daadcb2a54a726fabe67f30a9f9fb
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-02-24 20:34:46 +0200

    Add URL for logout

--- powersocket/urls.py
+++ powersocket/urls.py
@@ -8,5 +8,6 @@ from django.contrib.auth import views
 urlpatterns = [
     url(r'', include('main.urls')),
     url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'^admin/', admin.site.urls),
 ]

0532988c867880b01cf9da43747ee031b0898949
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-02-24 14:39:01 +0200

    Refactor func for brands menu

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -1,26 +1,24 @@
 
 function initBrandSelector() {
-  // look up select element with groups and attach our even handler
-  // on field "change" event
-  $('#brand-selector select').change(function(event){
-    // get value of currently selected group option
-    var brand = $(this).val();
+  $('.tab button').click(function(e){
+    e.preventDefault();
 
+    var brand = $(this).val();
     if (brand) {
-      // set cookie with expiration date 1 year since now;
-      // cookie creation function takes period in days
       $.cookie('current_brand', brand, {'path': '/', 'expires': 365});
+
     } else {
-      // otherwise we delete the cookie
       $.removeCookie('current_brand', {'path': '/'});
     }
 
-    // and reload a page
     location.reload(true);
+
+    $(this).addClass("active")
 
     return true;
   });
 }
+
 
 $(document).ready(function(){
   initBrandSelector();

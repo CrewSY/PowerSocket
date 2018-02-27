493eda61a3e8bf44f9d542b3ea23be4821d42f41
IhorDubas <ihordubas1525@gmail.com>
2018-02-27 11:10:22 +0200

    Add url, template link, view for basket

--- main/urls.py
+++ main/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^$', views.smartphones_list, name='home'),
     url(r'^search_smartphones/$', views.search_smartphones, name='search_smartphones'),
     url(r'^about_us/$', TemplateView.as_view(template_name='main/about_us.html')),
+    url(r'^basket/$', views.basket, name='basket'),
 ]

b396c3e8b28e09b0ef84a1a98ccda1a444d998aa
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-02-27 19:39:55 +0200

    Add models with additional fields for user
      Register model on admin page

--- userauth/admin.py
+++ userauth/admin.py
@@ -1,3 +1,25 @@
 """Management of admin panel."""
 
-from django.contrib import admin  # noqa: F401
+from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
+from django.contrib.auth.models import User
+
+from .models import UserProfile
+
+
+class UserProfileInline(admin.StackedInline):
+    """Additional fields to Order on admin page."""
+
+    model = UserProfile
+
+
+class UserAdmin(auth_admin.UserAdmin):
+    """Class that represents users at admin page."""
+
+    list_display = ('__str__', 'email', )
+    search_fields = ['__str__', 'email', ]
+    inlines = [UserProfileInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- userauth/models.py
+++ userauth/models.py
@@ -1,3 +1,33 @@
 """Models for PowerSocket project."""
 
-from django.db import models  # noqa: F401
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext as _
+
+
+class UserProfile(models.Model):
+    """Extra user data."""
+
+    class Meta(object):
+        """Meta data for user profile."""
+
+        verbose_name = _(u'User Profile')
+
+    user = models.OneToOneField(User)
+    full_name = models.CharField(verbose_name=_(u'Full name'),
+                                 max_length=64,
+                                 blank=True)
+
+    phone_number = models.CharField(verbose_name=_(u'Phone number'),
+                                    max_length=48,
+                                    blank=True)
+    address = models.CharField(verbose_name=_(u'Address'),
+                               max_length=128,
+                               blank=True)
+
+    def __str__(self):
+        """Render the user instance as a string."""
+        if self.full_name:
+            return self.full_name
+        else:
+            return self.user.username

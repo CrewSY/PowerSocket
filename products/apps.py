6c3a041c51314f02c8cd83c906d4e2a01d2a3e4b
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-02 08:41:06 +0200

    Initial commit of products app

---
+++ products/apps.py
@@ -0,0 +1,9 @@
+"""Configuration of app."""
+
+from django.apps import AppConfig
+
+
+class ProductsConfig(AppConfig):
+    """Configuration for products app of PowerSocket project."""
+
+    name = 'products'

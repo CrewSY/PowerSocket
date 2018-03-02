7a33c115a0c610dde8a1665139c770264d710e23
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-02 18:05:12 +0200

    Add js functions for updating quantity

--- main/static/js/quantity_goods.js
+++ main/static/js/quantity_goods.js
@@ -1,13 +1,45 @@
-var numCount = document.getElementById('num_count');
-var plusBtn = document.getElementById('button_plus');
-var minusBtn = document.getElementById('button_minus');
-plusBtn.onclick = function() {
-  var qty = parseInt(numCount.value);
-  qty = qty + 1;
-  numCount.value = qty;
+function initQuantity(){
+	$('.button_minus').click(function(e){
+        e.preventDefault();
+        var num_count = $(this).parent('.quantity_goods').find('.num_count');
+        var qty = parseInt(num_count.val());
+        var product_id = num_count.data("product_id");
+        var url = num_count.attr("action");
+        qty = qty - 1;
+        num_count.val(qty);
+        updateQuantity(qty, product_id, url);
+    });
+    $('.button_plus').click(function(e){
+        e.preventDefault();
+        var num_count = $(this).parent('.quantity_goods').find('.num_count');
+        var qty = parseInt(num_count.val());
+        var product_id = num_count.data("product_id");
+        var url = num_count.attr("action");
+        qty = qty + 1;
+        num_count.val(qty);
+        updateQuantity(qty, product_id, url);
+    });
+
 }
-minusBtn.onclick = function() {
-  var qty = parseInt(numCount.value);
-  qty = qty - 1;
-  numCount.value = qty;
+
+
+function updateQuantity(qty, product_id, url){
+    var data = {};
+    var csrf_token = $('#quantity_goods [name="csrfmiddlewaretoken"]').val();
+    data["csrfmiddlewaretoken"] = csrf_token;
+    data.product_id = product_id;
+    data.qty = qty;
+
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: data,
+        cache: true,
+    });
+
 }
+
+
+$(document).ready(function(){
+    initQuantity();
+});

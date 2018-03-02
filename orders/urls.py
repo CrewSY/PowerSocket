492883a1378312ed28f22daf3b396a3b7a7eebf8
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-02 18:04:10 +0200

    Add URL for updating quantity

--- orders/urls.py
+++ orders/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     url(r'^basket/$', views.basket, name='basket'),
     url(r'^basket_adding/$', views.basket_adding, name='basket_adding'),
+    url(r'^update_quantity/$', views.update_quantity, name='update_quantity'),
 ]

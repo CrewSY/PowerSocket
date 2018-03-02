199326130f810c26136bf4ce08cfaab633a5c9df
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-02 09:02:50 +0200

    Delete orders,products urls

--- main/urls.py
+++ main/urls.py
@@ -6,12 +6,6 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.products_list, name='home'),
-    url(r'^search_products/$', views.search_products, name='search_products'),
     url(r'^about_us/$', TemplateView.as_view(template_name='main/about_us.html')),
-    url(r'^basket/$', views.basket, name='basket'),
     url(r'^profile/$', views.profile, name='profile'),
-    url(r'^new_products/$', views.new_products, name='new_products'),
-    url(r'^basket_adding/$', views.basket_adding, name='basket_adding'),
-    url(r'^smartphone_details/$', views.smartphone_details, name='smartphone_details'),
 ]

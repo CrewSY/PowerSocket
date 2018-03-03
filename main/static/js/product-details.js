1cdd79fda40fc96616144555c28d49674e609c2f
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-03 15:28:11 +0200

    Add JS init functions for stars

--- main/static/js/product-details.js
+++ main/static/js/product-details.js
@@ -23,6 +23,76 @@ function initVotes(){
 }
 
 
+function initStar1(){
+    $('#vote-1').mouseover(function(e){
+        $(this).addClass('checked');
+    });
+    $('#vote-1').mouseleave(function(e){
+        $(this).removeClass('checked');
+    });
+}
+
+function initStar2(){
+    $('#vote-2').mouseover(function(e){
+        $('#vote-1').addClass('checked');
+        $(this).addClass('checked');
+    });
+    $('#vote-2').mouseleave(function(e){
+        $('#vote-1').removeClass('checked');
+        $(this).removeClass('checked');
+    });
+}
+
+function initStar3(){
+    $('#vote-3').mouseover(function(e){
+        $('#vote-1').addClass('checked');
+        $('#vote-2').addClass('checked');
+        $(this).addClass('checked');
+    });
+    $('#vote-3').mouseleave(function(e){
+        $('#vote-1').removeClass('checked');
+        $('#vote-2').removeClass('checked');
+        $(this).removeClass('checked');
+    });
+}
+
+function initStar4(){
+    $('#vote-4').mouseover(function(e){
+        $('#vote-1').addClass('checked');
+        $('#vote-2').addClass('checked');
+        $('#vote-3').addClass('checked');
+        $(this).addClass('checked');
+    });
+    $('#vote-4').mouseleave(function(e){
+        $('#vote-1').removeClass('checked');
+        $('#vote-2').removeClass('checked');
+        $('#vote-3').removeClass('checked');
+        $(this).removeClass('checked');
+    });
+}
+
+function initStar5(){
+    $('#vote-5').mouseover(function(e){
+        $('#vote-1').addClass('checked');
+        $('#vote-2').addClass('checked');
+        $('#vote-3').addClass('checked');
+        $('#vote-4').addClass('checked');
+        $(this).addClass('checked');
+    });
+    $('#vote-5').mouseleave(function(e){
+        $('#vote-1').removeClass('checked');
+        $('#vote-2').removeClass('checked');
+        $('#vote-3').removeClass('checked');
+        $('#vote-4').removeClass('checked');
+        $(this).removeClass('checked');
+    });
+}
+
 $(document).ready(function(){
     initVotes();
+    initStar1();
+    initStar2();
+    initStar3();
+    initStar4();
+    initStar5();
 });

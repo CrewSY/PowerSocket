ff609d0677c567f9c97e645e5fe38a8fd553ff55
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-03 15:11:40 +0200

    Add view for updating votes

--- orders/views.py
+++ orders/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 from orders.models import ProductInOrder, Order
+from products.models import Product
 
 
 @login_required
@@ -54,5 +55,20 @@ def remove_product(request):
 
     product = ProductInOrder.objects.filter(id=product_id)
     product.delete()
+
+    return HttpResponse()
+
+
+def update_rating(request):
+    """Update quantity of product in order."""
+    data = request.POST
+    product_id = data.get('product_id')
+    vote = data.get('vote')
+
+    product = Product.objects.get(id=product_id)
+    product.count_votes = product.count_votes + 1
+    product.rating = product.rating + int(vote)
+
+    product.save()
 
     return HttpResponse()

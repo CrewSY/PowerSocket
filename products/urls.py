9dd2d1e03e1af6c6af3cbfb6269e04cff59143b2
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-09 15:40:03 +0200

    Fix views

--- products/urls.py
+++ products/urls.py
@@ -6,9 +6,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.ProductList.as_view(), name='home'),
-    url(r'^search_products/$', views.search_products, name='search_products'),
     url(r'^new_products/$', views.ProductList.as_view(), name='new_products'),
     url(r'^rated_products/$', views.ProductList.as_view(), name='rated_products'),
+
     url(r'^product_details/(?P<pk>[0-9]+)/$', views.product_details, name='product_details'),
+
+    url(r'^search_products/$', views.search_products, name='search_products'),
     url(r'^update_content/$', views.UpdateContent.as_view(), name='update_content'),
 ]
--- products/views.py
+++ products/views.py
@@ -5,22 +5,19 @@ from django.shortcuts import render
 from django.views.generic import ListView
 
 
-from .models import Product, ProductBrand
-from orders.models import ProductInOrder, Order
-from .utils import paginate
+from .models import Product
+from .utils import get_data
 
 
-class ProductListView(ListView):
+class ProductList(ListView):
     """Render list of products according path."""
 
     model = Product
     template_name = 'main/products_list.html'
-    context_object_name = 'products'
-    paginate_by = 6
 
     def get_queryset(self):
         """Create queryset acording request.path."""
-        products = super(ProductListView, self).get_queryset()
+        products = super(ProductList, self).get_queryset()
 
         if '/new_products/' in self.request.path:
             products = products.order_by('-is_new')
@@ -29,14 +26,15 @@ class ProductListView(ListView):
 
         return products
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self):
         """Create context data."""
-        data = super().get_context_data(**kwargs)
-        data['brands'] = ProductBrand.objects.all().order_by('brand_name')
-        return data
+        context = get_data(self.request)
+        context['products'] = self.get_queryset()
+
+        return context
 
 
-class UpdateContentView(ListView):
+class UpdateContent(ListView):
     """Update content according filter products."""
 
     model = Product
@@ -44,9 +42,10 @@ class UpdateContentView(ListView):
 
     def get(self, request):
         """Render product content with filtered queryset."""
-        products = self.get_queryset()
+        context = self.get_context_data()
+        context['products'] = self.get_queryset()
 
-        return render(request, self.template_name, {'products': products})
+        return render(request, self.template_name, context)
 
     def get_queryset(self):
         """Create queryset according request."""
@@ -55,49 +54,37 @@ class UpdateContentView(ListView):
         if brand_id:
             products = Product.objects.filter(brand=brand_id)
         else:
-            products = super(UpdateContentView, self).get_queryset()
+            products = super(UpdateContent, self).get_queryset()
 
         return products
 
+    def get_context_data(self):
+        """Create context data."""
+        context = get_data(self.request)
+        context['products'] = self.get_queryset()
 
-def search_products(request, search_by):
-    """Update content according received data from search."""
+        return context
+
+
+def search_products(request):
+    """Update content according received data from input of search."""
+    search_by = request.GET.get('search_by')
     if search_by:
         q = Q()
-        for tag in search_by.split('_'):
+        for tag in search_by.split(' '):
             q |= Q(brand__brand_name__iexact=tag) | Q(model__iexact=tag)
         products = Product.objects.filter(q)
     else:
         products = Product.objects.all()
 
-    user = request.user
-    if user.is_authenticated():
-        try:
-            order = Order.objects.get(owner=user)
-            product_in_basket = ProductInOrder.objects.filter(order=order)
-            set_of_id = set()
-            for pr in product_in_basket:
-                set_of_id.add(pr.product.id)
-            context = paginate(products, 9,
-                               request,
-                               {'product_in_basket': set_of_id},
-                               var_name='products')
-        except Order.DoesNotExist:
-            context = paginate(products, 9,
-                               request,
-                               {'search_by': search_by},
-                               var_name='products')
-    else:
-        context = paginate(products, 9,
-                           request,
-                           {'search_by': search_by},
-                           var_name='products')
+    context = get_data(request)
+    context['products'] = products
 
     return render(request, 'main/product_content.html', context)
 
 
 def product_details(request, pk):
-    """Render page with product details."""
+    """Render page with details of choosen product."""
     product = Product.objects.get(id=pk)
     try:
         vote = int(product.rating/product.count_votes)

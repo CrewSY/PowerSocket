68f69158831deb5467e118ceda60e6114fad5435
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-09 12:32:00 +0200

    Update urls for class-based-view of list,update

--- products/urls.py
+++ products/urls.py
@@ -5,11 +5,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.products_list, name='home'),
+    url(r'^$', views.ProductListView.as_view(), name='home'),
     url(r'^search_products/(?P<search_by>.*)/$', views.search_products, name='search_products'),
-    url(r'^new_products/$', views.new_products, name='new_products'),
-    url(r'^rated_products/$', views.rated_products, name='rated_products'),
-    url(r'^discounted_products/$', views.discounted_products, name='discounted_products'),
+    url(r'^new_products/$', views.ProductListView.as_view(), name='new_products'),
+    url(r'^rated_products/$', views.ProductListView.as_view(), name='rated_products'),
     url(r'^product_details/(?P<pk>[0-9]+)/$', views.product_details, name='product_details'),
-    url(r'^update_content/(?P<pk>.*)/$', views.update_content, name='update_content'),
+    url(r'^update_content/$', views.UpdateContentView.as_view(), name='update_content'),
 ]

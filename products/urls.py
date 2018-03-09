781c95f206c7ad618e1f9267c5fbdd16687f770e
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-09 15:37:16 +0200

    Change url for search

--- products/urls.py
+++ products/urls.py
@@ -5,10 +5,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.ProductListView.as_view(), name='home'),
-    url(r'^search_products/(?P<search_by>.*)/$', views.search_products, name='search_products'),
-    url(r'^new_products/$', views.ProductListView.as_view(), name='new_products'),
-    url(r'^rated_products/$', views.ProductListView.as_view(), name='rated_products'),
+    url(r'^$', views.ProductList.as_view(), name='home'),
+    url(r'^search_products/$', views.search_products, name='search_products'),
+    url(r'^new_products/$', views.ProductList.as_view(), name='new_products'),
+    url(r'^rated_products/$', views.ProductList.as_view(), name='rated_products'),
     url(r'^product_details/(?P<pk>[0-9]+)/$', views.product_details, name='product_details'),
-    url(r'^update_content/$', views.UpdateContentView.as_view(), name='update_content'),
+    url(r'^update_content/$', views.UpdateContent.as_view(), name='update_content'),
 ]

18beca0dd883b9f8e27e51985097c68c1215b078
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-09 15:38:25 +0200

    Add get_data(), delete useless function

--- products/utils.py
+++ products/utils.py
@@ -1,51 +1,19 @@
 """
 Global functions for main app of PowerSocket project.
 
-  - def paginate(objects, size, request, context, var_name) -> context
-  - def get_brand(request) -> objects:brands
-  - def get_basket_products(request, products, brands) -> context
+  - def get_basket_products(request, products, brands) -> context data
   - def get_count_basket_products(request) -> count products
+
 """
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 from orders.models import ProductInOrder, Order
+from products.models import ProductBrand
 
 
-def paginate(objects, size, request, context, var_name='object_list'):
-    """Paginate objects."""
-    paginator = Paginator(objects, size)
-    page = request.GET.get('page', '1')
-    try:
-        object_list = paginator.page(page)
-    except PageNotAnInteger:
-        object_list = paginator.page(1)
-    except EmptyPage:
-        object_list = paginator.page(paginator.num_pages)
-
-    context[var_name] = object_list
-    context['is_paginated'] = object_list.has_other_pages()
-    context['page_obj'] = object_list
-    context['paginator'] = paginator
-
-    return context
-
-
-def get_brands(request):
-    """Return all brands."""
-    from .models import ProductBrand
-
-    brands = []
-    for brand in ProductBrand.objects.all().order_by('brand_name'):
-        brands.append({
-            'id': brand.id,
-            'brand_name': brand.brand_name,
-        })
-    return brands
-
-
-def get_basket_products(request, products, brands):
+def get_data(request):
     """Return context with paginated products."""
     user = request.user
-
+    brands = ProductBrand.objects.all().order_by('brand_name')
     if user.is_authenticated():
         try:
             order = Order.objects.get(owner=user)
@@ -53,21 +21,13 @@ def get_basket_products(request, products, brands):
             set_of_id = set()
             for pr in product_in_basket:
                 set_of_id.add(pr.product.id)
-            context = paginate(products, 6,
-                               request,
-                               {'product_in_basket': set_of_id,
-                                'brands': brands},
-                               var_name='products')
+            context = {'product_in_basket': set_of_id,
+                       'brands': brands}
         except Order.DoesNotExist:
-            context = paginate(products, 6,
-                               request,
-                               {'brands': brands},
-                               var_name='products')
+            context = {'brands': brands},
     else:
-        context = paginate(products, 6,
-                           request,
-                           {'brands': brands},
-                           var_name='products')
+        context = {'brands': brands}
+
     return context
 
 

df0ff126a89695fddc8bfdb35eab26d234d3eaca
Petrushynskyi Oleksii <petrushynskyi.oleksii@gmail.com>
2018-03-11 11:09:06 +0200

    Create function for gather data for upd profile

---
+++ webui/static/js/profile.js
@@ -0,0 +1,38 @@
+function updateUserData(){
+    $('#update-profile').click(function(event){
+        event.preventDefault();
+        var data = {};
+        data["csrfmiddlewaretoken"] = $('#profile-form [name="csrfmiddlewaretoken"]').val();
+        data["first_name"] = $('#first-name').val();
+        if(data.first_name == ''){
+            data["first_name"] = $('#first-name').attr('placeholder');
+        }
+        data["last_name"] = $('#last-name').val();
+        if(data.last_name == ''){
+            data["last_name"] = $('#last-name').attr('placeholder');
+        }
+        data["phone_number"] = $('#phone-number').val();
+        if(data.phone_number == ''){
+            data["phone_number"] = $('#phone-number').attr('placeholder');
+        }
+        data["address"] = $('#address').val();
+        if(data.address == ''){
+            data["address"] = $('#address').attr('placeholder');
+        }
+        console.log(data.address, data.phone_number, data.last_name, data.first_name)
+    });
+
+
+    // $.ajax({
+    //     url: url,
+    //     type: 'POST',
+    //     data: data,
+    //     cache: true,
+    //     success: location.reload(),
+    // });
+
+}
+
+$(document).ready(function(){
+    updateUserData();
+});

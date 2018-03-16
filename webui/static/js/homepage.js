613a46597e304d6e2e01316709339dd76b7a277a
IhorDubas <ihordubas1525@gmail.com>
2018-03-16 22:58:40 +0200

    Add adaptive for content

--- webui/static/js/homepage.js
+++ webui/static/js/homepage.js
@@ -11,3 +11,15 @@ function myFunction() {
   }
 }
 
+
+$(document).ready(function() {
+  $('.menu-trigger').click(function() {
+    $('nav ul').slideToggle(500);
+  });//end slide toggle
+
+  $(window).resize(function() {
+		if (  $(window).width() > 500 ) {
+			$('nav ul').removeAttr('style');
+		 }
+	});//end resize
+});//end ready
